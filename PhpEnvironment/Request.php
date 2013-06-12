233feb63e4b32706f73a3c86f0411647e74cab26
Matthew Weier O'Phinney <matthew@zend.com>
2013-06-12 14:47:24 -0500

    zendframework/zf2@99daaf515052dbdbc7d7f8b36c43d03de8b42e21 (1370405927)

--- PhpEnvironment/Request.php
+++ PhpEnvironment/Request.php
@@ -257,16 +257,7 @@ class Request extends HttpRequest
         // URI host & port
         $host = null;
         $port = null;
-
-        // Set the host
-        if ($this->getHeaders()->get('host')) {
-            $host = $this->getHeaders()->get('host')->getFieldValue();
-            // works for regname, IPv4 & IPv6
-            if (preg_match('|\:(\d+)$|', $host, $matches)) {
-                $host = substr($host, 0, -1 * (strlen($matches[1]) + 1));
-                $port = (int) $matches[1];
-            }
-        } elseif (isset($this->serverParams['SERVER_NAME'])) {
+        if (isset($this->serverParams['SERVER_NAME'])) {
             $host = $this->serverParams['SERVER_NAME'];
             if (isset($this->serverParams['SERVER_PORT'])) {
                 $port = (int) $this->serverParams['SERVER_PORT'];
@@ -280,6 +271,13 @@ class Request extends HttpRequest
                     // Unset the port so the default port can be used
                     $port = null;
                 }
+            }
+        } elseif ($this->getHeaders()->get('host')) {
+            $host = $this->getHeaders()->get('host')->getFieldValue();
+            // works for regname, IPv4 & IPv6
+            if (preg_match('|\:(\d+)$|', $host, $matches)) {
+                $host = substr($host, 0, -1 * (strlen($matches[1]) + 1));
+                $port = (int) $matches[1];
             }
         }
         $uri->setHost($host);
