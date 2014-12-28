zendframework/zf2

Allow HTTP client configuration to be supplied when using ClientStatic

tests/ZendTest/Http/Client/StaticClientTest.php
@@ -51,7 +51,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpSimpleGet()
     {
-        $response= HTTPClient::get($this->baseuri . 'testSimpleRequests.php');
+        $response = HTTPClient::get($this->baseuri . 'testSimpleRequests.php');
         $this->assertTrue($response->isSuccess());
     }
 
@@ -60,7 +60,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpGetWithParamsInUri()
     {
-        $response= HTTPClient::get($this->baseuri . 'testGetData.php?foo');
+        $response = HTTPClient::get($this->baseuri . 'testGetData.php?foo');
         $this->assertTrue($response->isSuccess());
         $this->assertContains('foo', $response->getBody());
     }
@@ -70,7 +70,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpMultiGetWithParam()
     {
-        $response= HTTPClient::get($this->baseuri . 'testGetData.php',array('foo' => 'bar'));
+        $response = HTTPClient::get($this->baseuri . 'testGetData.php',array('foo' => 'bar'));
         $this->assertTrue($response->isSuccess());
         $this->assertContains('foo', $response->getBody());
         $this->assertContains('bar', $response->getBody());
@@ -83,7 +83,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
     {
         $getBody = 'baz';
 
-        $response= HTTPClient::get($this->baseuri . 'testRawGetData.php',
+        $response = HTTPClient::get($this->baseuri . 'testRawGetData.php',
                                    array('foo' => 'bar'),
                                    array(),
                                    $getBody);
@@ -99,7 +99,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpSimplePost()
     {
-        $response= HTTPClient::post($this->baseuri . 'testPostData.php',array('foo' => 'bar'));
+        $response = HTTPClient::post($this->baseuri . 'testPostData.php',array('foo' => 'bar'));
         $this->assertTrue($response->isSuccess());
         $this->assertContains('foo', $response->getBody());
         $this->assertContains('bar', $response->getBody());
@@ -110,7 +110,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpPostContentType()
     {
-        $response= HTTPClient::post($this->baseuri . 'testPostData.php',
+        $response = HTTPClient::post($this->baseuri . 'testPostData.php',
                                     array('foo' => 'bar'),
                                     array('Content-Type' => Client::ENC_URLENCODED));
         $this->assertTrue($response->isSuccess());
@@ -125,12 +125,62 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
     {
         $postBody = 'foo';
 
-        $response= HTTPClient::post($this->baseuri . 'testRawPostData.php',
+        $response = HTTPClient::post($this->baseuri . 'testRawPostData.php',
                                     array('foo' => 'bar'),
                                     array('Content-Type' => Client::ENC_URLENCODED),
                                     $postBody);
 
         $this->assertTrue($response->isSuccess());
         $this->assertContains($postBody, $response->getBody());
+    }
+
+    /**
+     * Test GET with adapter configuration
+     *
+     * @link https://github.com/zendframework/zf2/issues/6482
+     */
+    public function testHttpGetUsesAdapterConfig()
+    {
+        $testUri = $this->baseuri . 'testSimpleRequests.php';
+
+        $config = array(
+            'useragent' => 'simplegettest'
+        );
+
+        HTTPClient::get($testUri, array(), array(), null, $config);
+
+        $reflectedClass = new \ReflectionClass('Zend\Http\ClientStatic');
+        $property = $reflectedClass->getProperty('client');
+        $property->setAccessible(true);
+        $client = $property->getValue();
+
+        $rawRequest = $client->getLastRawRequest();
+
+        $this->assertContains('User-Agent: simplegettest', $rawRequest);
+    }
+
+    /**
+     * Test POST with adapter configuration
+     *
+     * @link https://github.com/zendframework/zf2/issues/6482
+     */
+    public function testHttpPostUsesAdapterConfig()
+    {
+        $testUri = $this->baseuri . 'testPostData.php';
+
+        $config = array(
+            'useragent' => 'simpleposttest'
+        );
+
+        HTTPClient::post($testUri, array('foo' => 'bar'), array(), null, $config);
+
+        $reflectedClass = new \ReflectionClass('Zend\Http\ClientStatic');
+        $property = $reflectedClass->getProperty('client');
+        $property->setAccessible(true);
+        $client = $property->getValue();
+
+        $rawRequest = $client->getLastRawRequest();
+
+        $this->assertContains('User-Agent: simpleposttest', $rawRequest);
     }
 }

zendframework/zend-http

test/Client/StaticClientTest.php
@@ -50,7 +50,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpSimpleGet()
     {
-        $response= HTTPClient::get($this->baseuri . 'testSimpleRequests.php');
+        $response = HTTPClient::get($this->baseuri . 'testSimpleRequests.php');
         $this->assertTrue($response->isSuccess());
     }
 
@@ -59,7 +59,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpGetWithParamsInUri()
     {
-        $response= HTTPClient::get($this->baseuri . 'testGetData.php?foo');
+        $response = HTTPClient::get($this->baseuri . 'testGetData.php?foo');
         $this->assertTrue($response->isSuccess());
         $this->assertContains('foo', $response->getBody());
     }
@@ -69,7 +69,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpMultiGetWithParam()
     {
-        $response= HTTPClient::get($this->baseuri . 'testGetData.php', array('foo' => 'bar'));
+        $response = HTTPClient::get($this->baseuri . 'testGetData.php', array('foo' => 'bar'));
         $this->assertTrue($response->isSuccess());
         $this->assertContains('foo', $response->getBody());
         $this->assertContains('bar', $response->getBody());
@@ -82,7 +82,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
     {
         $getBody = 'baz';
 
-        $response= HTTPClient::get($this->baseuri . 'testRawGetData.php',
+        $response = HTTPClient::get($this->baseuri . 'testRawGetData.php',
                                    array('foo' => 'bar'),
                                    array(),
                                    $getBody);
@@ -98,7 +98,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpSimplePost()
     {
-        $response= HTTPClient::post($this->baseuri . 'testPostData.php', array('foo' => 'bar'));
+        $response = HTTPClient::post($this->baseuri . 'testPostData.php', array('foo' => 'bar'));
         $this->assertTrue($response->isSuccess());
         $this->assertContains('foo', $response->getBody());
         $this->assertContains('bar', $response->getBody());
@@ -109,7 +109,7 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttpPostContentType()
     {
-        $response= HTTPClient::post($this->baseuri . 'testPostData.php',
+        $response = HTTPClient::post($this->baseuri . 'testPostData.php',
                                     array('foo' => 'bar'),
                                     array('Content-Type' => Client::ENC_URLENCODED));
         $this->assertTrue($response->isSuccess());
@@ -124,12 +124,62 @@ class StaticClientTest extends \PHPUnit_Framework_TestCase
     {
         $postBody = 'foo';
 
-        $response= HTTPClient::post($this->baseuri . 'testRawPostData.php',
+        $response = HTTPClient::post($this->baseuri . 'testRawPostData.php',
                                     array('foo' => 'bar'),
                                     array('Content-Type' => Client::ENC_URLENCODED),
                                     $postBody);
 
         $this->assertTrue($response->isSuccess());
         $this->assertContains($postBody, $response->getBody());
+    }
+
+    /**
+     * Test GET with adapter configuration
+     *
+     * @link https://github.com/zendframework/zf2/issues/6482
+     */
+    public function testHttpGetUsesAdapterConfig()
+    {
+        $testUri = $this->baseuri . 'testSimpleRequests.php';
+
+        $config = array(
+            'useragent' => 'simplegettest'
+        );
+
+        HTTPClient::get($testUri, array(), array(), null, $config);
+
+        $reflectedClass = new \ReflectionClass('Zend\Http\ClientStatic');
+        $property = $reflectedClass->getProperty('client');
+        $property->setAccessible(true);
+        $client = $property->getValue();
+
+        $rawRequest = $client->getLastRawRequest();
+
+        $this->assertContains('User-Agent: simplegettest', $rawRequest);
+    }
+
+    /**
+     * Test POST with adapter configuration
+     *
+     * @link https://github.com/zendframework/zf2/issues/6482
+     */
+    public function testHttpPostUsesAdapterConfig()
+    {
+        $testUri = $this->baseuri . 'testPostData.php';
+
+        $config = array(
+            'useragent' => 'simpleposttest'
+        );
+
+        HTTPClient::post($testUri, array('foo' => 'bar'), array(), null, $config);
+
+        $reflectedClass = new \ReflectionClass('Zend\Http\ClientStatic');
+        $property = $reflectedClass->getProperty('client');
+        $property->setAccessible(true);
+        $client = $property->getValue();
+
+        $rawRequest = $client->getLastRawRequest();
+
+        $this->assertContains('User-Agent: simpleposttest', $rawRequest);
     }
 }