e5768d7d75938e49be37f1faeff2165095f03393
webimpress <contact@webimpress.com>
2017-03-05 08:37:55 +0000

    Short array syntax

--- test/Header/CookieTest.php
+++ test/Header/CookieTest.php
@@ -364,10 +364,10 @@ class CookieTest extends TestCase
 //        $expired = time() - 3600;
 //        $now = time() + 7200;
 //
-//        $cookies = array(
+//        $cookies = [
 //            'cookie=foo; domain=example.com; expires=' . date(DATE_COOKIE, $notexpired),
 //            'cookie=foo; domain=example.com; expires=' . date(DATE_COOKIE, $expired)
-//        );
+//        ];
 //
 //        // Make sure all cookies are expired
 //        foreach ($cookies as $cstr) {
@@ -427,11 +427,11 @@ class CookieTest extends TestCase
 //
 //    public function testGarbageInStrIsIgnored()
 //    {
-//        $cookies = array(
+//        $cookies = [
 //            'name=value; domain=foo.com; silly=place; secure',
 //            'foo=value; someCrap; secure; domain=foo.com; ',
 //            'anothercookie=value; secure; has some crap; ignore=me; domain=foo.com; '
-//        );
+//        ];
 //
 //        foreach ($cookies as $cstr) {
 //            $cookie = Http\Cookie::fromString($cstr);
@@ -461,15 +461,15 @@ class CookieTest extends TestCase
 //    {
 //        $uri = new Uri\Uri('http://www.foo.com/some/file.txt');
 //
-//        return array(
-//            array('foo=bar; domain=.example.com;', 'http://www.example.com/foo/bar.php', true),
-//            array('foo=bar; domain=.example.com;', 'http://example.com/foo/bar.php', true),
-//            array('foo=bar; domain=.example.com;', 'http://www.somexample.com/foo/bar.php', false),
-//            array('foo=bar; domain=example.com;', 'http://www.somexample.com/foo/bar.php', false),
-//            array('cookie=value; domain=www.foo.com', $uri, true),
-//            array('cookie=value; domain=www.foo.com', 'http://il.www.foo.com', true),
-//            array('cookie=value; domain=www.foo.com', 'http://bar.foo.com', false)
-//        );
+//        return [
+//            ['foo=bar; domain=.example.com;', 'http://www.example.com/foo/bar.php', true],
+//            ['foo=bar; domain=.example.com;', 'http://example.com/foo/bar.php', true],
+//            ['foo=bar; domain=.example.com;', 'http://www.somexample.com/foo/bar.php', false],
+//            ['foo=bar; domain=example.com;', 'http://www.somexample.com/foo/bar.php', false],
+//            ['cookie=value; domain=www.foo.com', $uri, true],
+//            ['cookie=value; domain=www.foo.com', 'http://il.www.foo.com', true],
+//            ['cookie=value; domain=www.foo.com', 'http://bar.foo.com', false],
+//        ];
 //    }
 //
 //    /**
@@ -570,16 +570,16 @@ class CookieTest extends TestCase
 //     */
 //    static public function invalidCookieNameCharProvider()
 //    {
-//        return array(
-//            array("="),
-//            array(","),
-//            array(";"),
-//            array("\t"),
-//            array("\r"),
-//            array("\n"),
-//            array("\013"),
-//            array("\014")
-//        );
+//        return [
+//            ["="],
+//            [","],
+//            [";"],
+//            ["\t"],
+//            ["\r"],
+//            ["\n"],
+//            ["\013"],
+//            ["\014"],
+//        ];
 //    }
 //
 //    /**
@@ -589,16 +589,16 @@ class CookieTest extends TestCase
 //     */
 //    static public function validCookieValueProvider()
 //    {
-//        return array(
-//            array('simpleCookie'),
-//            array('space cookie'),
-//            array('!@#$%^*&()* ][{}?;'),
-//            array("line\n\rbreaks"),
-//            array("0000j8CydACPu_-J9bE8uTX91YU:12a83ks4k"), // value from: Alexander Cheshchevik's comment on issue: ZF-1850
+//        return [
+//            ['simpleCookie'],
+//            ['space cookie'],
+//            ['!@#$%^*&()* ][{}?;'],
+//            ["line\n\rbreaks"],
+//            ["0000j8CydACPu_-J9bE8uTX91YU:12a83ks4k"], // value from: Alexander Cheshchevik's comment on issue: ZF-1850
 //
 //            // Long cookie value - 2kb
-//            array(str_repeat(md5(time()), 64))
-//        );
+//            [str_repeat(md5(time()), 64)],
+//        ];
 //    }
 //
 //    /**
@@ -608,13 +608,13 @@ class CookieTest extends TestCase
 //     */
 //    static public function refUrlProvider()
 //    {
-//        return array(
-//            array(new Uri\Uri('http://example.com/')),
-//            array(new Uri\Uri('http://www.example.com/foo/bar/')),
-//            array(new Uri\Uri('http://some.really.deep.domain.com')),
-//            array(new Uri\Uri('http://localhost/path/to/very/deep/file.php')),
-//            array(new Uri\Uri('http://arr.gr/some%20path/text%2Ffile'))
-//        );
+//        return [
+//            [new Uri\Uri('http://example.com/')],
+//            [new Uri\Uri('http://www.example.com/foo/bar/')],
+//            [new Uri\Uri('http://some.really.deep.domain.com')],
+//            [new Uri\Uri('http://localhost/path/to/very/deep/file.php')],
+//            [new Uri\Uri('http://arr.gr/some%20path/text%2Ffile')],
+//        ];
 //    }
 //
 //    /**
@@ -627,69 +627,68 @@ class CookieTest extends TestCase
 //        $now = time();
 //        $yesterday = $now - (3600 * 24);
 //
-//        return array(
-//            array(
+//        return [
+//            [
 //                'justacookie=foo; domain=example.com',
-//                array(
+//                [
 //                    'name'    => 'justacookie',
 //                    'domain'  => 'example.com',
 //                    'path'    => '/',
 //                    'expires' => null,
-//                    'secure'  => false
-//                )
-//            ),
-//            array(
+//                    'secure'  => false,
+//                ],
+//            ],
+//            [
 //                'expires=tomorrow; secure; path=/Space Out/; expires=Tue, 21-Nov-2006 08:33:44 GMT; domain=.example.com',
-//                array(
+//                [
 //                    'name'    => 'expires',
 //                    'domain'  => '.example.com',
 //                    'path'    => '/Space Out/',
 //                    'expires' => strtotime('Tue, 21-Nov-2006 08:33:44 GMT'),
-//                    'secure'  => true
-//                )
-//            ),
-//            array(
+//                    'secure'  => true,
+//                ],
+//            ],
+//            [
 //                'domain=unittests; expires=' . date(DATE_COOKIE, $now) . '; domain=example.com; path=/some%20value/',
-//                array(
+//                [
 //                    'name'    => 'domain',
 //                    'domain'  => 'example.com',
 //                    'path'    => '/some%20value/',
 //                    'expires' => $now,
 //                    'secure'  => false,
-//                )
-//            ),
-//            array(
+//                ],
+//            ],
+//            [
 //                'path=indexAction; path=/; domain=.foo.com; expires=' . date(DATE_COOKIE, $yesterday),
-//                array(
+//                [
 //                    'name'    => 'path',
 //                    'domain'  => '.foo.com',
 //                    'path'    => '/',
 //                    'expires' => $yesterday,
-//                    'secure'  => false
-//                )
-//            ),
-//
-//            array(
+//                    'secure'  => false,
+//                ],
+//            ],
+//            [
 //                'secure=sha1; secure; SECURE; domain=some.really.deep.domain.com',
-//                array(
+//                [
 //                    'name'    => 'secure',
 //                    'domain'  => 'some.really.deep.domain.com',
 //                    'path'    => '/',
 //                    'expires' => null,
-//                    'secure'  => true
-//                )
-//            ),
-//            array(
+//                    'secure'  => true,
+//                ],
+//            ],
+//            [
 //                'PHPSESSID=123456789+abcd%2Cef; secure; domain=.localdomain; path=/foo/baz; expires=Tue, 21-Nov-2006 08:33:44 GMT;',
-//                array(
+//                [
 //                    'name'    => 'PHPSESSID',
 //                    'domain'  => '.localdomain',
 //                    'path'    => '/foo/baz',
 //                    'expires' => strtotime('Tue, 21-Nov-2006 08:33:44 GMT'),
-//                    'secure'  => true
-//                )
-//            ),
-//        );
+//                    'secure'  => true,
+//                ],
+//            ],
+//        ];
 //    }
 //
 //    /**
@@ -700,13 +699,13 @@ class CookieTest extends TestCase
 //     */
 //    public static function cookieWithExpiredFlagProvider()
 //    {
-//        return array(
-//            array('cookie=foo;domain=example.com;expires=' . date(DATE_COOKIE, time() +  12 * 3600), false),
-//            array('cookie=foo;domain=example.com;expires=' . date(DATE_COOKIE, time() - 15), true),
-//            array('cookie=foo;domain=example.com;', false),
-//            // array('cookie=foo;domain=example.com;expires=Fri, 01-Mar-2109 00:19:21 GMT', false),
-//            // array('cookie=foo;domain=example.com;expires=Fri, 06-Jun-1966 00:19:21 GMT', true),
-//        );
+//        return [
+//            ['cookie=foo;domain=example.com;expires=' . date(DATE_COOKIE, time() +  12 * 3600), false],
+//            ['cookie=foo;domain=example.com;expires=' . date(DATE_COOKIE, time() - 15), true],
+//            ['cookie=foo;domain=example.com;', false],
+//            // ['cookie=foo;domain=example.com;expires=Fri, 01-Mar-2109 00:19:21 GMT', false],
+//            // ['cookie=foo;domain=example.com;expires=Fri, 06-Jun-1966 00:19:21 GMT', true],
+//        ];
 //    }
 // @codingStandardsIgnoreEnd
 }
