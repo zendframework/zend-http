f22afde7008d37e5baa0e3dd663bf337868a0890
Sergey.Klimov <klimser2@gmail.com>
2019-01-11 20:29:37 +0300

    Code style fix

--- src/Client/Adapter/Curl.php
+++ src/Client/Adapter/Curl.php
@@ -209,7 +209,7 @@ class Curl implements HttpAdapter, StreamInterface
             $connectTimeout = null;
         }
 
-        if ($connectTimeout !== null && !is_int($connectTimeout) && !is_numeric($connectTimeout)) {
+        if ($connectTimeout !== null && ! is_int($connectTimeout) && ! is_numeric($connectTimeout)) {
             throw new AdapterException\InvalidArgumentException(sprintf(
                 'integer or numeric string expected, got %s',
                 gettype($connectTimeout)
