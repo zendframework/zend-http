c2cdc44fe3969187e2d387911995592dffff5432
Eugene A. Gruzdev <yugeon.ru@gmail.com>
2019-08-22 16:43:08 +0300

    add test

---
+++ test/Client/_files/testProxyResponse.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "HTTP/1.1 200 Connection established\r\n\r\n";
+echo "HTTP/1.1 200 OK\r\n";
+echo "content-type: text/plain;charset=UTF-8\r\n";
+echo "server: Server\r\n";
+echo "\r\n";
+echo "work";
