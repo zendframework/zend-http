77b635976d6971b8f891d2d8a31f8b24deddf55b
Grzegorz Szymaszek <gszymaszek@short.pl>
2019-12-04 10:37:04 +0100

    Remove redundant “picture-in-picture” directive

--- src/Header/FeaturePolicy.php
+++ src/Header/FeaturePolicy.php
@@ -39,7 +39,6 @@ class FeaturePolicy implements HeaderInterface
         'midi',
         'payment',
         'picture-in-picture',
-        'picture-in-picture',
         'sync-xhr',
         'usb',
         'wake-lock',
